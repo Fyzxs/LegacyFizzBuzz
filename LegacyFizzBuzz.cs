8fcb59eff7e35ba7f01375ec71a45b8d1964d57a
Quinn <Fyzxs@users.noreply.github.com>
2018-04-25 15:16:32 -0700

    Create LegacyFizzBuzz.cs
    A legacy style FizzBuzz. It hurt a lot to create.

---
+++ LegacyFizzBuzz.cs
@@ -0,0 +1,65 @@
+using System;
+
+namespace FizzBuzzToRefactor
+{
+    [Flags]
+    public enum ModId
+    {
+        None = 0,
+        Three = 1,
+        Five = 2
+    }
+    public static class FizzBuzzer
+    {
+        public static string FizzBuzz(int problem)
+        {
+            IntAsString intAsString = new IntAsString();
+            intAsString.Value = problem;
+
+            ModId modId = ModId.None;
+            if (problem % 3 == 0)
+            {
+                modId = modId | ModId.Three;
+            }
+
+            if (problem % 5 == 0)
+            {
+                modId = modId | ModId.Five;
+            }
+
+            string response = UseEnums(modId);
+
+            if (response == null)
+            {
+                return intAsString.Convert(problem);
+            }
+            else
+            {
+                return response;
+            }
+        }
+
+        public static string UseEnums(ModId modId)
+        {
+            switch (modId)
+            {
+                case ModId.Three: return ModStrings.Three;
+                case ModId.Five: return ModStrings.Five;
+                case ModId.Three | ModId.Five: return ModStrings.Fifteen;
+                default: return null;
+            }
+        }
+    }
+
+    public class IntAsString
+    {
+        public int Value { get; set; }
+        public string Convert(int problem) => Value.ToString();
+    }
+    public static class ModStrings
+    {
+        public const string Three = "Fizz";
+        public const string Five = "Bµzz";
+        public const string Fifteen = Three + Five;
+    }
+}
