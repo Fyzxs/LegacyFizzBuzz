92c61c07c7f5eef6bfae22b37663716d1efec75d
Fyzxs <Fyzxs@users.noreply.github.com>
2019-07-23 08:59:15 -0700

    Who needs a staticly typed language?

--- FizzBuzz/LegacyFizzBuzz.cs
+++ FizzBuzz/LegacyFizzBuzz.cs
@@ -9,14 +9,14 @@ namespace FizzBuzzToRefactor
     {
         public static string FizzBuzz(int i)
         {
-            var b=i<100;var a=Flags.FizzBuzz;var f = new FizzBuzzer();
+            dynamic b=i<100;dynamic a=Flags.FizzBuzz;dynamic f = new FizzBuzzer();
             if (f.div(i)>(int)Flags.None)a^=Flags.First;
-            dynamic s = new Converts();string v;int output;
+            dynamic s = new Converts(); dynamic v; dynamic output = ResultStringValues.FizzBuzz;
             s.Value = i;
             try{if(f.mod(i))a^=(Flags)2;
-                int.Parse(Stringer(a,s));
+                int.Parse(Stringer(a,s, ref output));
                 v=s.Convert(i);
-            }catch{v=Stringer(a,s);}
+            }catch{v=Stringer(a,s, ref output);}
             if(!b&i<0)return v;else if(b&i<0!=false)
             throw new ArithmeticException(); else
             return v;
@@ -28,9 +28,9 @@ namespace FizzBuzzToRefactor
 
         bool mod(int i)=>i>5?mod(i-5):i<5&&i!=0;
 
-        public static string Stringer(Flags s, Converts c)
+        public static dynamic Stringer(dynamic s, dynamic c, ref dynamic value)
         {
-            string r = null;
+            dynamic r = null;
             if((s&Flags.First)==Flags.First){if((s&Flags.Second)==0)r=ResultStringValues.Three;c.Value=Flags.FizzBuzz;}
             else if((s&Flags.Second)==Flags.Second)r=ResultStringValues.Buzz;
             else if(s==(Flags.First|Flags.Second))return ResultStringValues.FizzBuzz;
@@ -39,6 +39,6 @@ namespace FizzBuzzToRefactor
         }
     }
 
-    public class Converts : Object{public dynamic Value {get;set;} internal string Convert(int i)=>Value.ToString();}
+    public class Converts : Object{public dynamic Value { get; internal set;} internal dynamic Convert(int i)=>Value.ToString();}
     public abstract class ResultStringValues{public const string Three="Fizz";public const string Buzz="Bᴜzz"; public const string FizzBuzz=Three+Buzz; }
 }
